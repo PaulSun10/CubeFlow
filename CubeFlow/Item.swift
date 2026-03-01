7cfc33e9368bc8a7a6ab1c3804c421556a16bea3
Paul Sun <3451425835@qq.com>
2026-03-02 01:28:19 +0800

    Initial Commit

---
+++ CubeFlow/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CubeFlow
+//
+//  Created by Paul Sun on 3/2/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CubeFlowTests/CubeFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  CubeFlowTests.swift
+//  CubeFlowTests
+//
+//  Created by Paul Sun on 3/2/26.
+//
+
+import Testing
+
+struct CubeFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
